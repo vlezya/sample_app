90fec2e0173f97f1e5146008b2ed5b97b35246a2
Stanislav Vlezko <vlezyashop@gmail.com>
2021-10-06 18:01:33 +0300

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,11 +3,11 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-                    password: "foobar", password_confirmation: "foobar") 
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
-      assert @user.valid?
+    assert @user.valid?
   end
 
   test "name should be present" do
@@ -31,8 +31,8 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should accept valid addresses" do
-    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org 
-                        first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
@@ -45,19 +45,23 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-  test "email addresses should be saved as lower-case" do 
+  test "email addresses should be saved as lower-case" do
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
-    assert_equal mixed_case_email.downcase, @user.reload.email 
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
-  test "password should have a minimum length" do 
-    @user.password = @user.password_confirmation = "a" * 5 
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?("")
   end
 end
